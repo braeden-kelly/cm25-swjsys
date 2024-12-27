ad04f4e6c4a193d71dfba3ecd4eb6c3752163c74
Brian Meeker <brian.meeker@getthru.io>
2024-12-27 16:26:39 -0500

    Ran the migration for habits

--- hotwire/db/schema.rb
+++ hotwire/db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_12_27_205703) do
+ActiveRecord::Schema[8.0].define(version: 2024_12_27_212519) do
+  create_table "habits", force: :cascade do |t|
+    t.string "name"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_habits_on_user_id"
+  end
+
   create_table "sessions", force: :cascade do |t|
     t.integer "user_id", null: false
     t.string "ip_address"
@@ -28,5 +36,6 @@ ActiveRecord::Schema[8.0].define(version: 2024_12_27_205703) do
     t.index ["email_address"], name: "index_users_on_email_address", unique: true
   end
 
+  add_foreign_key "habits", "users"
   add_foreign_key "sessions", "users"
 end
