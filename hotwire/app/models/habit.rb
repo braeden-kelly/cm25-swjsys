7d5188732d63da40203f8d3ba5ba9b3f8357214d
Brian Meeker <brian.meeker@getthru.io>
2024-12-28 15:24:21 -0500

    Create a new task automatically for today when a habit is created

--- hotwire/app/models/habit.rb
+++ hotwire/app/models/habit.rb
@@ -1,3 +1,11 @@
 class Habit < ApplicationRecord
   belongs_to :user, default: -> { Current.user }
+  has_many :tasks
+
+  after_create :create_task_for_today
+
+  private
+    def create_task_for_today
+      Task.create(user: self.user, habit: self, name: self.name, day: Date.today)
+    end
 end
